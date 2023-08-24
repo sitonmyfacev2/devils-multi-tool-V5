d9aceaf4f7a1c90b3a7fbbde62abd92a051f088c
sitonmyfavev2 <143049829+sitonmyfacev2@users.noreply.github.com>
2023-08-23 22:54:38 -0400

    Add files via upload

---
+++ devils multi tool V5/devilsrejects.bat
@@ -0,0 +1,87 @@
+@echo off
+chcp 65001   
+tittle DEVILS REJECTS
+mode 134,35
+color 4
+echo ▓█████▄ ▓█████ ██▒   █▓ ██▓ ██▓      ██████     ██▀███  ▓█████ ▄▄▄██▀▀▀▓█████  ▄████▄  ▄▄▄█████▓  ██████ 
+echo ▒██▀ ██▌▓█   ▀▓██░   █▒▓██▒▓██▒    ▒██    ▒    ▓██ ▒ ██▒▓█   ▀   ▒██   ▓█   ▀ ▒██▀ ▀█  ▓  ██▒ ▓▒▒██    ▒ 
+echo ░██   █▌▒███   ▓██  █▒░▒██▒▒██░    ░ ▓██▄      ▓██ ░▄█ ▒▒███     ░██   ▒███   ▒▓█    ▄ ▒ ▓██░ ▒░░ ▓██▄   
+echo ░▓█▄   ▌▒▓█  ▄  ▒██ █░░░██░▒██░      ▒   ██▒   ▒██▀▀█▄  ▒▓█  ▄▓██▄██▓  ▒▓█  ▄ ▒▓▓▄ ▄██▒░ ▓██▓ ░   ▒   ██▒
+echo ░▒████▓ ░▒████▒  ▒▀█░  ░██░░██████▒▒██████▒▒   ░██▓ ▒██▒░▒████▒▓███▒   ░▒████▒▒ ▓███▀ ░  ▒██▒ ░ ▒██████▒▒
+echo  ▒▒▓  ▒ ░░ ▒░ ░  ░ ▐░  ░▓  ░ ▒░▓  ░▒ ▒▓▒ ▒ ░   ░ ▒▓ ░▒▓░░░ ▒░ ░▒▓▒▒░   ░░ ▒░ ░░ ░▒ ▒  ░  ▒ ░░   ▒ ▒▓▒ ▒ ░
+echo  ░ ▒  ▒  ░ ░  ░  ░ ░░   ▒ ░░ ░ ▒  ░░ ░▒  ░ ░     ░▒ ░ ▒░ ░ ░  ░▒ ░▒░    ░ ░  ░  ░  ▒       ░    ░ ░▒  ░ ░
+echo  ░ ░  ░    ░       ░░   ▒ ░  ░ ░   ░  ░  ░       ░░   ░    ░   ░ ░ ░      ░   ░          ░      ░  ░  ░  
+echo    ░       ░  ░     ░   ░      ░  ░      ░        ░        ░  ░░   ░      ░  ░░ ░                     ░  
+echo  ░                 ░                                                          ░                          
+                   
+set /p IP=Enter faggots ip:
+chcp 65001   < code if text art Doesn't Work
+
+
+
+:rainbow
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo get fucked faggot ...)
+ping -t 2 0 10 127.0.0.1 >nul
+
+color a
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo get fucked faggot...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 02
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo get fucked faggot...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 03
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo get fucked faggot...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 04
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo get fucked faggot...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 05
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo get fucked faggot...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 06
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo get fucked faggot...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 07
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo get fucked faggot...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 08
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo get fucked faggot...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 09
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo get fucked faggote...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0a
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0b
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 0c
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 0d
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0e
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0f
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
+goto rainbow
---
+++ devils multi tool V5/hell.bat
@@ -0,0 +1,106 @@
+@echo off 
+chcp 65001 >nul
+mode 170,35
+:login
+color 9
+cls
+title devils rejects by .sitonmyface.
+echo ██╗      ██████╗  ██████╗ ██╗███╗   ██╗
+echo ██║     ██╔═══██╗██╔════╝ ██║████╗  ██║
+echo ██║     ██║   ██║██║  ███╗██║██╔██╗ ██║
+echo ██║     ██║   ██║██║   ██║██║██║╚██╗██║
+echo ███████╗╚██████╔╝╚██████╔╝██║██║ ╚████║
+echo ╚══════╝ ╚═════╝  ╚═════╝ ╚═╝╚═╝  ╚═══╝
+echo         made by .sitonmyface. 
+echo discord inv link https://discord.gg/9ng8PKcvrr
+echo  ---------------------------------------
+echo - username:go to discord               -
+echo -                                      -
+echo - passowrd:go to discord               -
+echo  ---------------------------------------
+
+set /p user=username:
+echo >nul
+set /p pass=password:
+if %user% == root  if %pass% == root  goto main
+echo Error 04, sorry it seems like u typed the wrong user or pass!
+timeout 2 >nul
+goto login
+:main
+echo welcome %USERNAME%
+cls 
+title devils rejects / by .sitonmyface.
+echo                                            ·▄▄▄▄   ▄▄▄ . ▌ ▐·▪   ▄▄▌  .▄▄ ·     ▄▄▄  ▄▄▄ . ▐▄▄▄▄▄▄ . ▄▄· ▄▄▄▄▄.▄▄ ·     
+echo                                             ██▪ ██ ▀▄.▀·▪█·█▌██ ██•  ▐█ ▀.     ▀▄ █·▀▄.▀·  ·██▀▄.▀·▐█ ▌▪•██  ▐█ ▀.     
+echo                                             ▐█· ▐█▌▐▀▀▪▄▐█▐█•▐█·██▪  ▄▀▀▀█▄    ▐▀▀▄ ▐▀▀▪▄▪▄ ██▐▀▀▪▄██ ▄▄ ▐█.▪▄▀▀▀█▄    
+echo                                             ██. ██ ▐█▄▄▌ ███ ▐█▌▐█▌▐▌▐█▄▪▐█    ▐█•█▌▐█▄▄▌▐▌▐█▌▐█▄▄▌▐███▌ ▐█▌·▐█▄▪▐█    
+echo                                             ▀▀▀▀▀•  ▀▀▀ . ▀  ▀▀▀.▀▀▀  ▀▀▀▀     .▀  ▀ ▀▀▀  ▀▀▀• ▀▀▀ ·▀▀▀  ▀▀▀  ▀▀▀▀  
+echo .                
+echo ===========================================================================================================================================================================
+echo -  1 pinger                                                                   11 random ip genarator 
+echo - 
+echo -  2 ip lookup                                                                12 danger virus maker 
+echo -       
+echo -  3 doxtool/lookup                                                           13 ascii art genarator FUN 
+echo -
+echo -  4 ip stresser/doxer                                                        14 phone call spoofer 
+echo -
+echo -  5 port scanner                                                             15 sms bomber FUN
+echo -
+echo -  6 fake info genarator FUN                                                  16 ip logger 
+echo -
+echo -  7 key word genarator tool                                                  17 follower tool all socials 
+echo -
+echo -  8 corvix/ovh                                                               18 insta id sercher 
+echo -
+echo -  9 info grabber                                                             19 search records and more info on people 
+echo -
+echo -  10 mrandom ip generator FUN                                                20 my discord link 
+echo -                                                         21 exit
+echo ==========================================================================================================================================================================
+echo. 
+:tools 
+set /p main= enter option 
+if %main% == 1 start devilsrejects.bat
+
+if %main% == 2 start a.exe
+
+if %main% == 3 start b.exe
+
+if %main% == 4 start https://ddoslist.net/hub
+
+if %main% == 5 start -.py
+
+if %main% == 6 start https://www.fakenamegenerator.com
+
+if %main% == 7 start https://keywordtool.io
+
+if %main% == 8 start https://courvix.com/email
+
+if %main% == 9 start https://www.vedbex.com/tools/home
+
+if %main% == 10 start https://commentpicker.com/ip-address-generator.php
+
+if %main% == 11 start https://minecraftalt.com
+
+if %main% == 12 start https://www.blackhost.xyz/?id=vm
+
+if %main% == 13 start https://patorjk.com/software/taag/#p=display&f=Elite&t=
+
+if %main% == 14 start https://www.spoofmyphone.com
+
+if %main% == 15 start https://bombitup.net
+
+if %main% == 16 start https://www.ps3cfw.com/iplog.php 
+
+if %main% == 17 start https://naizop.com
+
+if %main% == 18 start https://commentpicker.com/nl/instagram-gebruikers-id.php 
+
+if %main% == 19 start https://www.spokeo.com
+
+if %main% == 20 start https://discord.gg/9ng8PKcvrr
+
+if %main% == 21 exit 
+goto main
+ 
